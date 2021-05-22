b1d02a70069630ac0d6caf452e2f665b32a7547e
Jannakim <baradamoh@gmail.com>
2021-05-23 00:45:00 +0900

    chk amends: BOJ 1259

--- BOJ1259.py
+++ BOJ1259.py
@@ -1,14 +1,14 @@
 import sys
 input = sys.stdin.readline
 
-while(True):
+while(True): # while True:
     x = input()
-    if int(x[0]) == 0:
+    if int(x[0]) == 0: # if x=='0'
         break
     y = ''
-    for i in range(len(x)):
+    for i in range(len(x)): # 이렇게 뒤집어도 되지만 x[::-1] 하면 한방임. 파이썬만됨
         y += x[len(x) - i -1]
-    if(int(y) == int(x)):
+    if(int(y) == int(x)): # 숫자로 바꾸는 수고까진 필요 없을 것 같아요... >_<
         print("yes")
     else:
         print("no")
