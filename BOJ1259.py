a8bb3cd541c3b0875dd1e80d72d5959ae76781f4
Jannakim <baradamoh@gmail.com>
2021-05-23 00:46:26 +0900

    chk amends: BOJ 1259

--- BOJ1259.py
+++ BOJ1259.py
@@ -7,7 +7,7 @@ while(True): # while True:
         break
     y = ''
     for i in range(len(x)): # 이렇게 뒤집어도 되지만 x[::-1] 하면 한방임. 파이썬만됨
-        y += x[len(x) - i -1]
+        y += x[len(x) - i -1] # 추가: len(x) 따로 변수로 빼서 쓰면 더 좋을 것 같아요
     if(int(y) == int(x)): # 숫자로 바꾸는 수고까진 필요 없을 것 같아요... >_<
         print("yes")
     else:
