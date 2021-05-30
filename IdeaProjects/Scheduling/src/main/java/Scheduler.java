7e42075b630fceb23c3e6fd0a342cef99f8c4d06
cokkum113 <84273370+cokkum113@users.noreply.github.com>
2021-05-30 19:26:20 +0900

    delete.IdeaProjects

--- IdeaProjects/Scheduling/src/main/java/Scheduler.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public interface Scheduler
-{
-    public boolean setProcess(ArrayList<Process> prs);
-    public Process nextProcess(int currentTime);
-    public int execute(int pid);
-    public  boolean isEmpty();
-}
